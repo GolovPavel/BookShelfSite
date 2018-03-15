a57307fa2186a2d40f6ed80102a77199819b9116
golovpavel <golovpasha@gmail.com>
2018-03-16 01:22:10 +0300

    Add admin pages for models

--- book_shelf/main_app/admin.py
+++ book_shelf/main_app/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from main_app.models import Book
+from main_app.models import Note
+from main_app.models import Like
+from main_app.models import Comment
+from main_app.models import Book_rating
+from main_app.models import User_to_book
+
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+class NoteAdmin(admin.ModelAdmin):
+    pass
+
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+class Book_ratingAdmin(admin.ModelAdmin):
+    pass
+
+class User_to_bookAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Book_rating, Book_ratingAdmin)
+admin.site.register(User_to_book, User_to_bookAdmin)
