9873bd18e2f4b8b97f7a6bffc2bff4dbe22c5024
golovpavel <golovpasha@gmail.com>
2018-03-29 11:32:22 +0300

    Add template page, url, view for welcome page

--- book_shelf/book_shelf/settings.py
+++ book_shelf/book_shelf/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'files', 'static'),
+)
 STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
 MEDIA_URL = '/media/'
